ffb37fedb319e1ee1c2e4e3973278b0d534496d1
rChrisb <5767@holbertonstudents.com>
2023-05-25 15:13:58 +0200

    camera movement with gsap

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ import * as dat from "dat.gui";
 import moon from "../images/moon.jpg";
 import earth from "../images/earth.jpg";
 import * as TWEEN from "tween.js";
+import { gsap } from "/node_modules/gsap/index";
 
 // set the render
 const renderer = new THREE.WebGLRenderer();
@@ -30,7 +31,7 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.enableZoom = true;
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
-camera.position.set(-10, 30, 30);
+camera.position.set(0, 0, -10);
 orbit.update();
 
 // lighting
@@ -55,6 +56,7 @@ const sphereMaterial = new THREE.MeshStandardMaterial({
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.position.set(10, 10, 10);
 
 // particules array
 const particles = [];
@@ -87,6 +89,24 @@ for (let i = 0; i < numberOfParticules; i++) {
   createParticle();
 }
 
+// renderer.domElement.addEventListener("mousedown", function () {
+//   gsap.to(camera.position, {
+//     duration: 1,
+//     z: camera.position.z - 10,
+//     ease: "power3.inOut",
+//   });
+//   console.log("hi");
+// });
+
+window.addEventListener("mousedown", function () {
+  gsap.to(camera.position, {
+    z: camera.position.z - 10,
+    duration: 1.5,
+    onUpdate: function () {
+      camera.lookAt(0, 0, 0);
+    },
+  });
+});
 // Event listener for mouse scroll
 // document.addEventListener("wheel", function (event) {
 //   event.preventDefault();
@@ -117,48 +137,7 @@ for (let i = 0; i < numberOfParticules; i++) {
 //     .add(zoomDirection.multiplyScalar(zoomAmount));
 //   camera.position.copy(newPosition);
 // });
-let isMouseDown = false;
-let initialMousePosition = new THREE.Vector2();
 
-// Event listeners for mouse down and up
-document.addEventListener("mousedown", function (event) {
-  isMouseDown = true;
-  initialMousePosition.x = event.clientX;
-  initialMousePosition.y = event.clientY;
-});
-
-document.addEventListener("mouseup", function () {
-  isMouseDown = false;
-});
-
-// Event listener for mouse move
-document.addEventListener("mousemove", function (event) {
-  if (isMouseDown) {
-    const currentMousePosition = new THREE.Vector2(
-      event.clientX,
-      event.clientY
-    );
-    const delta = new THREE.Vector2().subVectors(
-      currentMousePosition,
-      initialMousePosition
-    );
-    const zoomSpeed = 0.1; // Adjust the zoom speed here
-    const zoomAmount = delta.y * zoomSpeed;
-
-    // Calculate the zoom factor based on the mouse movement
-    const zoomFactor = Math.pow(0.95, zoomAmount) * 1.1;
-
-    // Zoom towards the specific spot
-    const zoomDirection = camera.position.clone().normalize();
-    const newPosition = camera.position
-      .clone()
-      .add(zoomDirection.multiplyScalar(zoomAmount));
-    camera.position.copy(newPosition);
-
-    // Update the initial mouse position for the next frame
-    initialMousePosition.copy(currentMousePosition);
-  }
-});
 // animation of the scene
 function animate() {
   sphere.rotation.z += 0.02;

d7b5c7cf66e571fffeabd8a0a8827f3e2ebdab90
rChrisb <5767@holbertonstudents.com>
2023-06-22 16:36:00 +0200

    test

--- src/js/script.js
+++ src/js/script.js
@@ -110,7 +110,7 @@ const planet3Button = {
   previous: "planet 1",
   succeededRace: true,
   minimum_score: false,
-  scoreNeededPreviously: 5000,
+  scoreNeededPreviously: 500,
   color: "yellow",
   script: new URL("/game-2d?level=2", window.location.href).href,
 };
